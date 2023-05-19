b58a0f7497ffbca0f114ea0c9c8c73fb55c4db3f
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-05-19 08:44:26 +0200

    relative url bug repro

--- autorun.js
+++ autorun.js
@@ -1,30 +1,25 @@
 Office.initialize = function (reason) { };
 
+/**
+ * Handles the OnNewMessageCompose event.
+ */
 function onNewMessageComposeHandler(event) {
+    var xmlhttp = new XMLHttpRequest();
     
-    Office.context.roamingSettings.set("test-key", "test-value");
-
-    Office.context.roamingSettings.saveAsync(function(asyncResult) {      
-        var xmlhttp = new XMLHttpRequest();
-
-        xmlhttp.onload = () => {
-            insertDebugSignature("CORS OK: " + xmlhttp.responseText, event);
-        };
-
-        xmlhttp.onerror = () => {
-            insertDebugSignature(xmlhttp.responseText, event)
-        }
-
-        xmlhttp.open("POST", "https://jsonplaceholder.typicode.com/posts");
-        xmlhttp.setRequestHeader("Content-Type", "application/json");
-        xmlhttp.setRequestHeader("Authorization", "Bearer test-token");
-        xmlhttp.send(JSON.stringify({test: "value"}));
-    });
-}
-
-function insertDebugSignature(text, event) {
-    var signature = `<strong style='font-size: 16px;'> ${text} </strong>`;
-    Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
+    xmlhttp.onload = function() {
+        var signature = `<strong style='font-size: 20px; font-color: greeen'>Success: ${xmlhttp.responseText} </strong>`;
+        Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
+    }
+    
+    xmlhttp.onerror = function() {
+        var signature = `<strong style='font-size: 20px; font-color: red'>Error: ${xmlhttp.responseText} </strong>`;
+        Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
+    }
+    
+    xmlhttp.open('GET', '/api/get', true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    
+    xmlhttp.send();
 }
 
 Office.actions.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
